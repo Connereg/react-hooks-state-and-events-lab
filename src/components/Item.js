112dc142671ae166e5815178b5a977461855cdc1
Conner Gleason <connereg@optonline.net>
2021-09-01 11:10:59 -0400

    Completed Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
+
+
 
 function Item({ name, category }) {
+  const [isAdded, setAddedToCart] = useState(false)
+  function addToCartFunction(){
+    setAddedToCart((isAdded) => !isAdded);
+  }
+
+  const addedItem = isAdded ? "in-cart" : ""
+  const buttonColorChange = isAdded ? "remove" : "add"
+  const buttonTextChange = isAdded ? "Added To Cart!" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={addedItem}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCartFunction} className={buttonColorChange}>{buttonTextChange}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCatagory, setSelectedCatagory] = useState("All");
+
+  function handleCatagoryChange(event) {
+    // event.target.value will be the value selected by the user
+    setSelectedCatagory(event.target.value);
+  }
+
+  // we want to filter the items to only display the ones based on the selected category
+  const itemsToDisplay = items.filter((item) => {
+    if(selectedCatagory === "All") return true;
+
+    return item.category === selectedCatagory;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleCatagoryChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +28,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
